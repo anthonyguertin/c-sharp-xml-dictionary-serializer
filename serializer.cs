65e860dbeafe2b4647113e289ec1f9cc65181ac8
Anthony Guertin <anthony.guertin@gmail.com>
2017-09-24 22:52:22 -0600

    Update serializer.cs

--- serializer.cs
+++ serializer.cs
@@ -5,7 +5,7 @@ using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
-namespace SwiftExcel.Planswift.XmlSerializers
+namespace Serializers.XmlSerializers
 {
   [Serializable]
   [XmlRoot("dictionary")]
